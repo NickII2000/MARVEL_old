fd8d46e44821e24ae18719fe4d0d4bce9606c0a2
Nick <Nick_II2000@mail.ru>
2022-11-02 08:33:09 +0600

    12-th commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import './style/style.scss';
 const marvelService = new MarvelService();
 
 // marvelService.getAllCharacters().then(res => console.log(res));
-
 marvelService.getCharacter(1011052).then(res => console.log(res));
 
 ReactDOM.render(
