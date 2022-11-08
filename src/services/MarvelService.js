ec62b17576abacd1c0826f8bb6a93b51971f1f4f
Nick <Nick_II2000@mail.ru>
2022-11-08 06:58:44 +0600

    47-th commit

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -20,7 +20,7 @@ class MarvelService {
 
     getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        console.log(res.data.results[0]);
+        // console.log(res.data.results[0]);
         return this._transformCharacter(res.data.results[0]);
     }
 
