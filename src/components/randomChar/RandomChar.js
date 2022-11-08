f05b6d1ba6e81786bd1a8dd8e4d527ae247ff69d
Nick <Nick_II2000@mail.ru>
2022-11-08 20:13:31 +0600

    60-th commit

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -18,17 +18,18 @@ class RandomChar extends Component {
 
     marvelService = new MarvelService();
 
-    onChatLoaded = (char) => {
-        console.log(char); // временно
-        console.log({ char }); // временно
-        this.setState({ char });
+    onCharLoaded = (char) => {
+        this.setState({
+            char,
+            loading: false
+        });
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
             .getCharacter(id)
-            .then(this.onChatLoaded);
+            .then(this.onCharLoaded);
     }
 
     render() {
