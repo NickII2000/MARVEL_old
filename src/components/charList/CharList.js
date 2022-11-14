aca63578010749faebd620fdd34b0ec51a3df94b
Nick <Nick_II2000@mail.ru>
2022-11-14 08:25:43 +0600

    93-th commit

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -46,9 +46,7 @@ class CharList extends Component {
             }
 
             return (
-                <li
-                    className="char__item"
-                    key={item.id}>
+                <li className="char__item" key={item.id}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className="char__name">{item.name}</div>
                 </li>
