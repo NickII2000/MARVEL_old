7ef9758a2ec6542e0f7004f68619df17d2e92723
Nick <Nick_II2000@mail.ru>
2022-11-16 20:57:42 +0600

    115-th commit

---
+++ src/components/errorBuondary/ErrorBoundary.js
@@ -0,0 +1,23 @@
+import { Component } from "react/cjs/react.production.min";
+
+class ErrorBoundary extends Component {
+    state = {
+        error: false,
+    }
+
+    componentDidCatch(error, errorInfo) {
+        console.log(error, errorInfo);
+        this.setState({
+            error: true,
+        })
+    }
+
+    render() {
+        if (this.state.error) {
+            return <h2>something went wrong</h2>;
+        }
+
+        return this.props.children;
+    }
+}
+
