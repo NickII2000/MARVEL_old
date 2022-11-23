16d6cd48b238c2e734715cc9f76f113cbab76c81
Nick <Nick_II2000@mail.ru>
2022-11-24 00:29:18 +0600

    145-th commit

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,7 +12,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 15,
+        offset: 1544,
         charEnded: false,
     }
 
