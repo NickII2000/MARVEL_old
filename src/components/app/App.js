a3c97d389023eef3bef55cef74fbb5a443f05f66
Nick <Nick_II2000@mail.ru>
2022-12-05 19:28:44 +0600

    191-th commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -128,7 +128,8 @@ const Wrapper = styled.div`
 const DynamicGreating = (props) => {
     return (
         <div className={'mb-3 p-3 border-' + props.color} className={'mb-3 p-3 border-' + props.color} >
-            {props.children}
+            {/* {props.children} */}
+
         </div >
     )
 
